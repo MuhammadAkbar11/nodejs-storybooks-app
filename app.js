7fce5fc861d5c08ff214f77142105721c59aa3cd
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-12-09 15:32:01 +0700

    Morgan logger

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const dotenv = require("dotenv");
 const path = require("path");
+const morgan = require("morgan");
 const express = require("express");
 const connectDB = require("./config/db");
 // const connectDB = require
@@ -10,6 +11,10 @@ dotenv.config({ path: "./config/config.env" });
 connectDB();
 
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 const PORT = process.env.PORT || 3000;
 const MODE = process.env.NODE_ENV;
