1778ab49349cc6c4faa3208b3f15af437c326107
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-12-10 01:37:03 +0700

    create User Model

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  googleId: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+});
+
+const UserModel = mongoose.model("User", UserSchema, "users");
+
+module.exports = UserModel;
