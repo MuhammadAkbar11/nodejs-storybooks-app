ec49c68a2e4e0e339baece92425e8f5dda11cb37
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-12-10 01:34:47 +0700

    passport Strategy

--- config/passport.js
+++ config/passport.js
@@ -1,2 +1,27 @@
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const Mongoose = require("mongoose");
+const UserModel = require("../models/User");
+
+module.exports = function (passport) {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "/auth/google/callback",
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        console.log(profile);
+      }
+    )
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  passport.deserializeUser((id, done) => {
+    UserModel.findById(id, (err, user) => {
+      done(err, user);
+    });
+  });
+};
