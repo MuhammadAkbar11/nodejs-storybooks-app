1c7affc7bc3b64caeb3e89d2c687f8257d969b8f
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-12-09 18:59:05 +0700

    setup webpack

---
+++ webpack/webpack.config.js
@@ -0,0 +1,82 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  entry: {
+    srcipt: path.resolve(__dirname, "../src/index.js"),
+  },
+  devtool: "source-map",
+  watch: true,
+  watchOptions: {
+    ignored: /node_modules/,
+    aggregateTimeout: 200,
+    poll: 1000,
+  },
+  mode: "production",
+  output: {
+    filename: "js/[name].min.js",
+    chunkFilename: "js/chunk-[name].[contenthash].js",
+    path: path.join(__dirname, "..", "assets/dist"),
+  },
+  resolve: {
+    extensions: [".css", ".scss", ".js", ".jsx", ".json"],
+    alias: {
+      "@scss": path.resolve(__dirname, "..", "src/scss"),
+      "~bootstrap": path.resolve(__dirname, "..", "node_modules/bootstrap"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /node_modules/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          "css-loader",
+          "sass-loader",
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.(svg|png|jpg|jpeg|gif)(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name][ext]",
+        },
+      },
+      {
+        test: /\.(eot|gif|otf|ttf|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name][ext]",
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new OptimizeCssAssetsPlugin(),
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          compress: true,
+        },
+      }),
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].min.css",
+    }),
+  ],
+};
