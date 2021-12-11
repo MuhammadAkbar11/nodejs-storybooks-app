89ceea96f95b3015ecc7d6909e6968512b096e1d
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-12-11 23:15:49 +0700

    make timestamp to true

--- models/User.js
+++ models/User.js
@@ -1,26 +1,29 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  googleId: {
-    type: String,
-    required: true,
+const UserSchema = new mongoose.Schema(
+  {
+    googleId: {
+      type: String,
+      required: true,
+    },
+    displayName: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
   },
-  displayName: {
-    type: String,
-    required: true,
-  },
-  firstName: {
-    type: String,
-    required: true,
-  },
-  lastName: {
-    type: String,
-  },
-  image: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
-const UserModel = mongoose.model("User", UserSchema, "users");
+const UserModel = mongoose.model("UserModel", UserSchema, "users");
 
 module.exports = UserModel;
