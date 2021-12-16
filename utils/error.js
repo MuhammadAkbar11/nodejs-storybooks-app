a46cf0cf08a50b0508137b30be268cb074e5af7a
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-12-16 20:29:55 +0700

    replace errors property with data

--- utils/error.js
+++ utils/error.js
@@ -1,9 +1,21 @@
-function ErrorResponse(statusCode, message, errors = {}) {
+function ErrorResponse({
+  statusCode = 500,
+  message = "Something went wrong",
+  data = {},
+  ...errors
+}) {
   this.statusCode = statusCode;
   this.message = message;
-  this.errors = { ...errors };
-  this.message = message || "Something went wrong";
+  this.data = { ...data };
+  this.message = message;
   this.stack = new Error().stack;
+
+  this.errors = { ...errors };
+
+  if (errors.kind === "ObjectId" || errors.statusCode) {
+    this.statusCode = 404;
+    this.message = "Product not found";
+  }
 }
 
 ErrorResponse.prototype = Object.create(Error.prototype);
