06155ce3ae6c44f7088e5c0b65c49a8ef4b0fd75
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-12-17 17:03:02 +0700

    finishing landing page

--- routes/story.js
+++ routes/story.js
@@ -14,16 +14,16 @@ const { ensureAuth } = require("../middleware/auth");
 const router = express.Router();
 
 router.get("/", getPublicStories);
+
+router
+  .route("/add")
+  .get(ensureAuth, getAddStory)
+  .post(ensureAuth, postAddStory);
+router.get("/edit/:id", ensureAuth, getEditStory);
 router.get("/user/:id", getUserStories);
 router
   .route("/:id")
   .get(getSingleStory)
   .put(ensureAuth, putEditStory)
   .delete(ensureAuth, deleteStory);
-router
-  .route("/add")
-  .get(ensureAuth, getAddStory)
-  .post(ensureAuth, postAddStory);
-router.get("/edit/:id", ensureAuth, getEditStory);
-
 module.exports = router;
