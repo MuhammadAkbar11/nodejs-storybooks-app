4cb3f6863c95e1e5f41288c3ca13475fb6b57f3d
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-12-26 11:29:14 +0700

    mongodb connect config for low connection

--- utils/contants.js
+++ utils/contants.js
@@ -1,8 +1,10 @@
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
 
+const devMode2 = process.argv.find(arg => arg.includes("--dev2"));
+
 exports.PORT = process.env.PORT || 3000;
-exports.MONGO_URI = process.env.MONGO_URI;
+exports.MONGO_URI = devMode2 ? process.env.MONGO_URI2 : process.env.MONGO_URI;
 exports.GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 exports.GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 exports.DEV_MODE = "development";
